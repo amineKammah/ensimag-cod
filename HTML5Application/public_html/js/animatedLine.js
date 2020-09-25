5ee525ac943b54ca39e0517c7306ef8846e13603
Kaoutar Hafyane <kaoutar.hafyane@grenoble-inp.org>
2020-09-25 10:59:07 +0200

    animatedline

--- HTML5Application/public_html/js/animatedLine.js
+++ HTML5Application/public_html/js/animatedLine.js
@@ -1,10 +1,9 @@
-// set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("#animatedLine")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -12,48 +11,32 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
+var xLine = d3.scaleBand()
+    .range([0, 800])
+    .padding(0.1);
+var yLine = d3.scaleLinear()
+    .range([100, 0]);
+var line = d3.line()
+    .x(d => xLine(d.year))
+    .y(d => yLine(d.n));
+var data = d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/5_OneCatSevNumOrdered.csv", function(data) {
 
-  // group the data: I want to draw one line per group
-  var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
-    .key(function(d) { return d.name;})
-    .entries(data);
+    xLine.domain(data.map(d => d.year));
+    yLine.domain(d3.extent(data, d => d.n));
+    var linePath = svg.append("path")
+        .datum(data)
+        .attr("d", line)
+        .style("fill", "none")
+        .style("stroke", "#3498db")
+        .style("stroke-width", "1px")
+        .attr("transform", "translate(150, 0)");
 
-  // Add X axis --> it is a date format
-  var x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.year; }))
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).ticks(5));
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return +d.n; })])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // color palette
-  var res = sumstat.map(function(d){ return d.key }) // list of group names
-  var color = d3.scaleOrdinal()
-    .domain(res)
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-
-  // Draw the line
-  svg.selectAll(".line")
-      .data(sumstat)
-      .enter()
-      .append("path")
-        .attr("fill", "none")
-        .attr("stroke", function(d){ return color(d.key) })
-        .attr("stroke-width", 1.5)
-        .attr("d", function(d){
-          return d3.line()
-            .x(function(d) { return x(d.year); })
-            .y(function(d) { return y(+d.n); })
-            (d.values)
-        })
-
-})
+    var linePathLength = linePath.node().getTotalLength(); // LIGNE 20
+    linePath
+        .attr("stroke-dasharray", linePathLength)
+        .attr("stroke-dashoffset", linePathLength)
+        .transition()
+            .duration(4000)
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0);
+});
